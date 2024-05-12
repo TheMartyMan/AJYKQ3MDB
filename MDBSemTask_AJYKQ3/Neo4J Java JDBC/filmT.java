654e0d07f7573c2d5dfd960ab6c4ac8ae2abdc64
GMárton <martonstar@gmail.com>
2024-05-12 13:18:56 +0200

    Create filmT.java

---
+++ MDBSemTask_AJYKQ3/Neo4J Java JDBC/filmT.java
@@ -0,0 +1,19 @@
+package neo4j;
+
+public class filmT {
+	String id;
+	String name;
+	int releaseDate;
+	String director;
+	int price;
+	
+	
+	public filmT(String id, String name, int releaseDate, String director, int price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.releaseDate = releaseDate;
+		this.director = director;
+		this.price = price;
+	}
+}
